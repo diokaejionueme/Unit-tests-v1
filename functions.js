b1e98ec8e375779c56f5e1f74d2a4ed34f79c2db
Dioka Ejionueme <diokajr@gmail.com>
2022-06-15 21:10:15 +0100

    Finally getting it to work

--- functions.js
+++ functions.js
@@ -1,9 +1,35 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(a, b) {
+    return a + b
 }
+
+function sayHello(name){
+    if(name !== 'Will')
+    return(`Hi there ${name}!`);
+    else{
+
+        return(`No more testing ${name}!`);
+    }
+}
+
+function buildCar(){
+
+    for(var i = 0; i < arguments.length; i++){
+    if(arguments.length === 1){
+        return arguments[0];
+    }else if(arguments.length === 2){
+        return arguments[2];
+
+    }else{
+        return({});
+    }
+}
+}
+
+
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
